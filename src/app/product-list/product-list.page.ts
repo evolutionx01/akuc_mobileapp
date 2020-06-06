43cc2cd778dc99e3c11dd8a5ad4af3fccaf4aa04
Sraveen Nagunuri <sraveen.nagunuri@hotmail.com>
2020-06-06 23:27:05 +0530

    =product list design

--- src/app/product-list/product-list.page.ts
+++ src/app/product-list/product-list.page.ts
@@ -7,9 +7,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListPage implements OnInit {
 
+   public total_list_items:any =25;
+
+  
+   public  productlist :any[]=[
+    {
+      "imageUrl":"/assets/images/mango.png",
+      "brandName":"Fresho",
+      "branditem":"Banganapalli",
+      "mrpPrices":"15.50",
+      "offerPrices":"10.50",
+      "quantitycol":[{
+        "itemCount":1
+      },
+      {
+        "itemCount":3
+     },
+     {
+      "itemCount":5
+     }]   
+    },
+    {
+      "imageUrl":"/assets/images/mango3.png",
+      "brandName":"Fresho",
+      "branditem":"Mangoes",
+      "mrpPrices":"15.50",
+      "offerPrices":"10.50",
+      "quantitycol":[{
+        "itemCount":1
+      },
+      {
+        "itemCount":3
+     },
+     {
+      "itemCount":5
+     }]   
+    },
+    
+    {
+      "imageUrl":"/assets/images/mango1.png",
+      "brandName":"Fresho",
+      "branditem":"Mangoes",
+      "mrpPrices":"15.50",
+      "offerPrices":"10.50",
+      "quantitycol":[{
+        "itemCount":1
+      },
+      {
+        "itemCount":3
+     },
+     {
+      "itemCount":5
+     }]   
+    },
+    {
+      "imageUrl":"/assets/images/mango3.png",
+      "brandName":"Fresho",
+      "branditem":"Mangoes",
+      "mrpPrices":"15.50",
+      "offerPrices":"10.50",
+      "quantitycol":[{
+        "itemCount":1
+      },
+      {
+        "itemCount":3
+     },
+     {
+      "itemCount":5
+     }]   
+    },
+    {
+      "imageUrl":"/assets/images/mango.png",
+      "brandName":"Fresho",
+      "branditem":"Mangoes",
+      "mrpPrices":"15.50",
+      "offerPrices":"10.50",
+      "quantitycol":[{
+        "itemCount":1
+      },
+      {
+        "itemCount":3
+     },
+     {
+      "itemCount":5
+     }]   
+    },
+
+   ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public cartCount: number = 0;
+  
+  handleCart(type) {
+    if (type === "add") {
+      this.cartCount++
+    }
+    else {
+      this.cartCount--
+    }
   }
 
 }
