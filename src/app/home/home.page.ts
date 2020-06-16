68b063ed5aaae2ea4d5618bbb19353e18f1a5238
aravinth <evolutionx01@gmail.com>
2020-06-16 19:13:58 +0530

    my chnages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true
+   };
+
+   topStories = [
+     {image : 'https://picsum.photos/id/1/200/300',
+    title: 'heage'},
+     {image : 'https://picsum.photos/id/10/200/300',
+     title: 'heage'},
+     {image : 'https://picsum.photos/id/100/200/300',
+     title: 'heage'}
+    ]
 
   constructor() { }
 
